4e00092c63b6e67f0dc4785d53dc6d88b984f6b0
Rado <rado@masabi.com>
2021-12-01 16:46:51 +0000

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,30 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1Internal(input: List<Int>): Int {
+        return input.foldIndexed(0) { index, acc, value ->
+            if (index == 0 || value <= input[index -1]) acc
+            else acc + 1
+        }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        return part1Internal(input)
+    }
+
+    fun part2(input: List<Int>): Int {
+        val newValues = input.mapIndexed { index: Int, value: Int ->
+            if (index + 2 >= input.size) null
+            else value + input[index + 1] + input[index + 2]
+        }.filterNotNull()
+
+        return part1Internal(newValues)
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01_test").map { it.toInt() }
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
-    val input = readInput("Day01")
+    val input = readInput("Day01").map { it.toInt() }
     println(part1(input))
     println(part2(input))
 }
