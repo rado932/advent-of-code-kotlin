9afb8e7fc9273c17384711ea496fde015e9c1ea7
Rado <rado@masabi.com>
2025-12-01 16:53:24 +0000

    Y2025 Day1 - extracted methods

--- src/y2025/Day01.kt
+++ src/y2025/Day01.kt
@@ -9,20 +9,12 @@ private const val triggerPos = 0
 private fun String.toDelta() = replace("R", "+").replace("L", "-").toInt()
 
 fun main() {
+
+    // This is and the Edge case not covered by the other method
+    fun hasPassedThreshold(pos: Int, cur: Int): Boolean = pos != triggerPos && cur <= 0
+    fun calculateAdditionalPasses(cur: Int): Int = (abs(cur) / maxPos)
+
     fun part1(input: List<String>): Int {
-        var pos = startPos
-        var passOnZero = 0
-
-        input.forEach {
-            val cur = pos + it.toDelta()
-            pos = cur.mod(maxPos)
-            if (pos == triggerPos) passOnZero++
-        }
-
-        return passOnZero
-    }
-
-    fun part1Map(input: List<String>): Int {
         var pos = startPos
         return input.map {
             val cur = pos + it.toDelta()
@@ -33,25 +25,9 @@ fun main() {
 
     fun part2(input: List<String>): Int {
         var pos = startPos
-        var passOnZero = 0
-
-        input.forEach {
-            val cur = pos + it.toDelta()
-
-            if (pos != triggerPos && cur <= 0) passOnZero++
-            passOnZero += abs(cur) / maxPos
-
-            pos = cur.mod(maxPos)
-        }
-
-        return passOnZero
-    }
-
-    fun part2Map(input: List<String>): Int {
-        var pos = startPos
         return input.sumOf {
             val cur = pos + it.toDelta()
-            ((abs(cur) / maxPos) + (if (pos != triggerPos && cur <= 0) 1 else 0)).also {
+            ((if (hasPassedThreshold(pos, cur)) 1 else 0) + calculateAdditionalPasses(cur)).also {
                 pos = cur.mod(maxPos)
             }
         }
@@ -61,11 +37,11 @@ fun main() {
 
     val input = File("$inputPrefix.txt").readLines()
 
-    val testAnswerPart1 = part1Map(testInput)
+    val testAnswerPart1 = part1(testInput)
     check(testAnswerPart1 == 3, { "part 1 failed: $testAnswerPart1" })
-    time { println(part1Map(input)) } // 1172
+    time { println(part1(input)) } // 1172
 
-    val testAnswerPart2 = part2Map(testInput)
+    val testAnswerPart2 = part2(testInput)
     check(testAnswerPart2 == 6, { "part 2 failed: $testAnswerPart2" })
-    time { println(part2Map(input)) } // 6932
+    time { println(part2(input)) } // 6932
 }
