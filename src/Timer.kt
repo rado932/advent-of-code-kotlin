b5bce93db01db26e5eabbd670f2d63bd363ecdf6
Rado <rado@masabi.com>
2025-12-02 14:42:42 +0000

    Y2025 Updated Timer to use measureTime

--- src/Timer.kt
+++ src/Timer.kt
@@ -1,35 +1,9 @@
-import java.text.DecimalFormat
-import java.time.Instant
-import java.time.format.DateTimeFormatter
-import java.util.*
-import kotlin.random.Random
-import kotlin.reflect.KClass
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTime
 
 object Timer {
-    private val map: MutableMap<String, Long> = mutableMapOf()
-
-    fun time(block: ()-> Any) {
-        val name = Random.nextLong().toString()
-        start(name)
-        block()
-        stop(name)
-    }
-
-    private fun start(name: String) {
-        map[name] = System.currentTimeMillis()
-    }
-
-    private fun stop(name: String) {
-        println(
-            formatMillisecondsAsSeconds(
-                System.currentTimeMillis() - map[name]!!
-            )
-        )
-    }
-
-    private fun formatMillisecondsAsSeconds(milliseconds: Long): String {
-        val seconds = milliseconds / 1000.0
-        val decimalFormat = DecimalFormat("#.###")
-        return decimalFormat.format(seconds) + "s"
+    @OptIn(ExperimentalTime::class)
+    fun time(block: () -> Any) {
+        println(measureTime { block() })
     }
 }
